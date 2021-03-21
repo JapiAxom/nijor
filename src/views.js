0bee9d6d24f0343ea58df9a530f1a98433652bdc
DebAxom <debaxom@gmail.com>
2021-03-21 12:28:40 +0530

    Tweaked the views.js file

--- src/views.js
+++ src/views.js
@@ -1,5 +1,6 @@
-export default function(newVal,view){
+export const setView = (newVal,view)=>{
     document.querySelectorAll('nijorview[view="'+view+'"]').forEach(function(child){
         child.innerHTML=newVal;
     });
 }
+export const getView = view => document.querySelector('nijorview[view="'+view+'"]').innerHTML;
