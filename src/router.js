084f993895e38c3af4d4402eda3aab8c6b73af93
DebAxom <debaxom@gmail.com>
2021-03-27 15:15:18 +0530

    Fixed a bug

--- src/router.js
+++ src/router.js
@@ -12,24 +12,7 @@ export default class{
         }
     }
     render(url,Page){
-        let url_with_shlash,url_without_shlash;
-        if(url.endsWith('/')){
-            url_with_shlash = url;
-            let url_chars_array = url.split('').reverse();
-            let new_url_chars_array = url_chars_array.pop();
-            url_without_shlash = new_url_chars_array.join('');
-        }else{
-            url_with_shlash = url + '/';
-            url_without_shlash = url;
-        }
-        allRoutes[url_with_shlash] = ()=>{
-            setTimeout(()=>{
-                document.querySelector(this.routesDiv).innerHTML="<app></app>";
-                Page.init('app');
-                Page.run();
-        },3);
-        };
-        allRoutes[url_without_shlash] = ()=>{
+        allRoutes[url] = ()=>{
             setTimeout(()=>{
                 document.querySelector(this.routesDiv).innerHTML="<app></app>";
                 Page.init('app');
