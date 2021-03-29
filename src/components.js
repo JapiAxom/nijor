70f26513a769297fc254ee0fe28723ca0556672f
DebAxom <debaxom@gmail.com>
2021-03-29 13:24:30 +0530

    Fixed a bug in rendering components.

--- src/components.js
+++ src/components.js
@@ -22,17 +22,19 @@ export default class{
         this.name = name;
     }
     run(){
-        let to_be_replaced = new RegExp(`(<${this.name}[^>]+>|<${this.name}>)`);
-        document.querySelectorAll(this.name).forEach((child) => {
-                let allSpecs = child.getAttributes();
-                this.template(allSpecs).then((result)=>{
-                    try {
-                        child.parentElement.innerHTML = child.parentElement.innerHTML.replace(to_be_replaced,result);
-                    } catch (error) {
-                        document.body.innerHTML = document.body.innerHTML.replace(to_be_replaced,result);
-                    }
+        setTimeout(()=>{
+            let to_be_replaced = new RegExp(`(<${this.name}[^>]+>|<${this.name}>)`);
+            document.querySelectorAll(this.name).forEach((child) => {
+                    let allSpecs = child.getAttributes();
                     child.innerHTML='';
+                    this.template(allSpecs).then((result)=>{
+                        try {
+                            child.parentElement.innerHTML = child.parentElement.innerHTML.replace(to_be_replaced,result);
+                        } catch (error) {
+                            document.body.innerHTML = document.body.innerHTML.replace(to_be_replaced,result);
+                        }
+                });
             });
-        });
+        },3);
     }
 }
