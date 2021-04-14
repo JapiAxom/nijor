674a32b18dfad91594dfd53e4aa5f4c92d995225
DebAxom <debaxom@gmail.com>
2021-04-14 13:25:47 +0530

    Fixed a bug in nijor-events.js

--- src/nijor-events.js
+++ src/nijor-events.js
@@ -6,7 +6,11 @@ export default class {
         document.querySelectorAll('[on'+this.eventName+']').forEach(element=>{
             let cevent = element.getAttribute('on'+this.eventName);
             var ceventArr = cevent.split(')');
-            ceventArr[1]=','+JSON.stringify(data)+')';
+            if(ceventArr[0].endsWith('(')){
+                ceventArr[1]=JSON.stringify(data)+')';
+            }else{
+                ceventArr[1]=','+JSON.stringify(data)+')';
+            }
             cevent = ceventArr.join('');
             eval(cevent);
         });
