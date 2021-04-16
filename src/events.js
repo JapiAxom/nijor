c1ce5bef62f414d8b120556df90169d4fed35e12
DebAxom <debaxom@gmail.com>
2021-04-16 14:02:56 +0530

    Tweaked the events.js file

--- src/events.js
+++ src/events.js
@@ -1,18 +1,13 @@
-export default function(eventName){
-    return({
-        eventName:eventName,
-        emit:function(data){
-            document.querySelectorAll('[on'+this.eventName+']').forEach(element=>{
-                let cevent = element.getAttribute('on'+this.eventName);
-                var ceventArr = cevent.split(')');
-                if(ceventArr[0].endsWith('(')){
-                    ceventArr[1]=JSON.stringify(data)+')';
-                }else{
-                    ceventArr[1]=','+JSON.stringify(data)+')';
-                }
-                cevent = ceventArr.join('');
-                eval(cevent);
-            });
+export function emit(eventName,data){
+    document.querySelectorAll('[on'+eventName+']').forEach(element=>{
+        let cevent = element.getAttribute('on'+eventName);
+        var ceventArr = cevent.split(')');
+        if(ceventArr[0].endsWith('(')){
+            ceventArr[1]=JSON.stringify(data)+')';
+        }else{
+            ceventArr[1]=','+JSON.stringify(data)+')';
         }
+        cevent = ceventArr.join('');
+        eval(cevent);
     });
 }
