cc78413354f1dac2eac13d58c954d5beabb23dab
DebAxom <debaxom@gmail.com>
2021-04-20 14:48:26 +0530

    Updated the components.js file

--- src/components.js
+++ src/components.js
@@ -5,26 +5,22 @@ export default class{
     init(name){
         this.name = name;
     }
-    run(){
+    run = async function(){
         let to_be_replaced = new RegExp(`(<${this.name}[^>]+>|<${this.name}>)`);
         let element = document.getElementsByTagName(this.name)[0];
         if(document.getElementsByTagName(this.name).length===0) return;
         let allSpecs = element.getAttributes();
         try {
             element.innerHTML="";
-            this.template(allSpecs).then(result=>{
-
+            try {
+                let result = await this.template(allSpecs);
                 element.parentElement.innerHTML = element.parentElement.innerHTML.replace(to_be_replaced,result);
                 this.run();
-
-            }).catch(e=>{
-
-                this.template(allSpecs).then(result=>{
-                    document.body.innerHTML = document.body.innerHTML.replace(to_be_replaced,result);
-                    this.run();
-                });
-               
-            });
+            } catch (error) {
+                let result = await this.template(allSpecs);
+                document.body.innerHTML = document.body.innerHTML.replace(to_be_replaced,result);
+                this.run();
+            }
         } catch (error) {}
     }
 }
