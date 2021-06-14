338458d4b7f228a73d2ddbaa8692cf56c31fbe38
DebAxom <debaxom@gmail.com>
2021-06-14 13:06:36 +0530

    Changed some files.

--- src/nijor-electron.js
+++ src/nijor-electron.js
@@ -1,25 +1,4 @@
-import "./nijor.common-runtime.js";
-
-window.addEventListener('popstate',async e =>{
-    let path = e.target.location.pathname;
-
-    if(path.indexOf(':')>-1){
-    path = path.split(':')[1];
-    }
-
-    let hash = e.target.location.hash;
-    if (hash === "" || hash === "#") {
-        hash = "#";
-    }
-    if(path===window.nijor.previousRoute){
-        await window.nijor.renderHashRoute(window.nijor.previousRoute,hash);
-        return;
-    }
-    await window.nijor.renderRoute(path);
-    await window.nijor.renderHashRoute(path,hash);
-    window.nijor.previousRoute = path;
-});
-
+import "./nijor.js";
 history.pushState(null,null,'/');
 history.pushState(null,null,'/');
 history.back();
--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ async function get(url){
 let response = await fetch(url);
 return(response.json());
 }
-async function post(url,data,headers){
+async function post(url,data,headers={"Content-type": "application/json"}){
 let response = await fetch(url,{
     method:"POST",
     body:JSON.stringify(data),
@@ -10,7 +10,7 @@ let response = await fetch(url,{
 });
 return(response.json());
 }
-async function put(url,data,headers){
+async function put(url,data,headers={"Content-type": "application/json"}){
 let response = await fetch(url,{
     method:"PUT",
     body:JSON.stringify(data),
@@ -18,7 +18,7 @@ let response = await fetch(url,{
 });
 return(response.json());
 }
-async function del(url,headers){
+async function del(url,headers={"Content-type": "application/json"}){
 let response = await fetch(url,{
     method:"DELETE",
     headers:headers
